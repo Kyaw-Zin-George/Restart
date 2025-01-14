22a45300b186f79d55092e9f30d1df8c99039462
Kyaw-Zin-George <ktz1695@gmail.com>
2025-01-14 05:43:42 +0000

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Kyaw Thant Zin(George) on 1/14/25.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
